b2324a29da83d5bbbe264ae029fcd230c2183c93
Bennett Perkins <whatever@bennettp123.com>
2014-04-29 11:51:23 +0800

    Add usage text; remove hardcoded paths

--- dns.he.net_updater.sh
+++ dns.he.net_updater.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+USAGE="Usage:
+ $(basename $0) -h <fqdn> -p <password> [-l] [i <iface>]
+ $(basename $0) --help
+
+ -h <fqdn>     : The hostname to update
+ -p <password> : The password to use
+ -l            : Set using local interface address
+ -i <iface>    : The interface to use when getting local address. Implies -l
+
+By default, $(basename $0) queries dns.he.net to determine the public IP address.
+When called with -l or -i, it uses the address of a local interface instead.
+"
+
 url="https://dyn.dns.he.net/nic/update"
 previous_file_prefix=~/.dns.he.net
 
@@ -10,7 +23,12 @@ password=''
 use_local_iface_address='' # set this to 'yes' to use ifconfig to determine local IP addresses.
 iface='eth0' # ignored unless $use_local_iface_address = yes
 
-while getopts 'h:p:li:' optname; do
+if [ "$1" == "--help" ]; then
+  printf "%s\n" "$USAGE"
+  exit 0
+fi
+
+while getopts 'h:p:li:h' optname; do
   case "$optname" in
     'h')
       hostname="$OPTARG"
@@ -22,14 +40,17 @@ while getopts 'h:p:li:' optname; do
       use_local_iface_address='yes'
       ;;
     'i')
+      use_local_iface_address='yes'
       iface="$OPTARG"
       ;;
     ':')
       echo "No argument value for option $OPTARG"
+      printf "%s\n" "$USAGE"
       exit 1
       ;;
     *)
       echo 'Unknown error while processing options'
+      printf "%s\n" "$USAGE"
       exit 1
       ;;
   esac
@@ -45,30 +66,30 @@ if [ "$use_local_iface_address" == "yes" ]; then
   elif which ifconfig >/dev/null 2>&1; then
     currentip=$(ifconfig en0 inet | grep -E '^.*inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}.*$' | sed -E 's/^.*inet ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*$/\1/')
   else
-    /usr/bin/logger -i -t com.bennettp123.dyndns "${hostname}: could not determine local IP address"
+    logger -i -t com.bennettp123.dyndns "${hostname}: could not determine local IP address"
     retval=1
     break
   fi
 else
-  currentip=$(/usr/bin/curl -4 -s "http://checkip.dns.he.net" | /bin/grep -iE "Your ?IP ?address ?is ?: ?" | /bin/sed -r 's/.*\s+([[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}).*/\1/')
+  currentip=$(curl -4 -s "http://checkip.dns.he.net" | grep -iE "Your ?IP ?address ?is ?: ?" | sed -r 's/.*\s+([[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}).*/\1/')
 fi
 
 oldip=$(echo "${previous}" \
-          | /bin/grep "${hostname}" \
-          | /bin/sed -r 's/.*\s+([[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}).*/\1/')
+          | grep "${hostname}" \
+          | sed -r 's/.*\s+([[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}).*/\1/')
 
 if [ "_$oldip" = "_" ]; then
   oldip="unknown"
 fi
 
 if [ "_$currentip" != "_$oldip" ]; then
-  /usr/bin/logger -i -t com.bennettp123.dyndns "${hostname}: old ip: ${oldip}; current ip: ${currentip}; updating..."
-  result=$(/usr/bin/curl -4 -s "${url}" -d "hostname=${hostname}" -d "password=${password}")
+  logger -i -t com.bennettp123.dyndns "${hostname}: old ip: ${oldip}; current ip: ${currentip}; updating..."
+  result=$(curl -4 -s "${url}" -d "hostname=${hostname}" -d "password=${password}")
   retval=$?
-  /usr/bin/logger -i -t com.bennettp123.dyndns "${hostname}:${result1}"
+  logger -i -t com.bennettp123.dyndns "${hostname}:${result1}"
   echo "${hostname}:${result1}" > "${previous_file}"
 else
-  /usr/bin/logger -i -t com.bennettp123.dyndns "${hostname}: old ip: ${oldip}; current ip: ${currentip}; not updating"
+  logger -i -t com.bennettp123.dyndns "${hostname}: old ip: ${oldip}; current ip: ${currentip}; not updating"
   retval=0
 fi
 
